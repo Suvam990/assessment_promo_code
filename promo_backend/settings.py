983307a64307f01599990450cc6102c9788429a1
Suvam990 <subhamlama13@gmail.com>
2025-05-24 00:06:47 +0545

    Add Render app domain to ALLOWED_HOSTS

--- promo_backend/settings.py
+++ promo_backend/settings.py
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'subham-promo-handler.onrender.com',
+]
